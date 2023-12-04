6accafda7be803880f79f54f136ee0702e3d1d6c
Mirf2404 <113890864+Mirf2404@users.noreply.github.com>
2023-12-04 10:23:01 +0100

    Delete PracticaCarmelo directory

--- PracticaCarmelo/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    require('classes/Request.php');
-    require('classes/Files.php');
-    
-    $image = Request::request('image');
-    Files::forceDownload($image, substr(basename($image), 15 ));
-    
-?>
--- PracticaCarmelo/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require('classes/Request.php');
-    require('classes/Files.php');
-
-    $image = Request::request('image');
-    
-        Files::file($image);
-  
